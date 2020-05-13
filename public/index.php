9e728ebcf855e0ac66b7882635329cc9741087a2
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2020-05-14 01:50:12 +0700

    update

--- public/index.php
+++ public/index.php
@@ -16,23 +16,32 @@ if (! in_array($currentIp, $whitelist)) {
 }
 
 if ($url = $_REQUEST['url'] ?? false) {
-    $file = Uuid::uuid4().'.pdf';
+    $file = 'pdf/'.Uuid::uuid4().'.pdf';
     $browser = Browsershot::url($url);
     if ($path = getenv('NODE_PATH')) {
         $browser->setIncludePath($path);
     }
-    $browser
-        ->noSandbox()
-        ->waitUntilNetworkIdle()
-        ->windowSize(1200, 900)
-        ->showBrowserHeaderAndFooter()
-        ->delay($_REQUEST['delay'] ?? 3000)
-        ->margins(12, 5, 12, 5)
-        ->pages($_REQUEST['pages'] ?? '')
-        ->format($_REQUEST['size'] ?? 'A4')
-        ->save($file);
     
-    header('Location:/'.$file);
+    try {
+        $browser
+            ->noSandbox()
+            ->waitUntilNetworkIdle($_REQUEST['strict'] ?? 1)
+            ->windowSize(1200, 900)
+            ->showBrowserHeaderAndFooter()
+            ->delay($_REQUEST['delay'] ?? 3000)
+            ->margins(12, 5, 12, 5)
+            ->pages($_REQUEST['pages'] ?? '')
+            ->format($_REQUEST['size'] ?? 'A4')
+            ->save('./'.$file);
+
+        if (file_exists(__DIR__.'/'.$file)) {
+            header('Location:/'.$file);
+            die();
+        }
+    } catch (\Exception $e) {}
+
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    die('500 Internal Server Error');
 }
 
 die('no URL provided');
