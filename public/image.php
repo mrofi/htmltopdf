b61097d233858068dd0ba7872daafc70ba615d1c
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2020-08-01 01:19:21 +0700

    add more parameters in image

--- public/image.php
+++ public/image.php
@@ -27,27 +27,36 @@ if (! in_array($currentIp, $whitelist)) {
 }
 
 if ($url = $_REQUEST['url'] ?? false) {
-    $file = Uuid::uuid4().'.png';
     $browser = Browser::url($url);
     if ($path = getenv('NODE_PATH')) {
         $browser->setIncludePath($path);
     }
 
+    $type = $_REQUEST['type'] ?? 'png';
+    $type = $type == 'jpeg' || $type == 'jpg' ? 'jpeg' : 'png';
+
+    $quality = $type == 'jpeg' ? $_REQUEST['quality'] ?? 80 : null;
+    $width = $_REQUEST['width'] ?? 800;
+    $height = $_REQUEST['height'] ?? 800;
+
     try {
         $browser
             ->noSandbox()
+            ->setScreenshotType($type, (int) $quality)
             ->waitUntilNetworkIdle($_REQUEST['strict'] ?? 1)
-            ->windowSize(800, 800)
+            ->windowSize((int) $width, (int) $height)
             ->hideBrowserHeaderAndFooter();
 
         if ($_REQUEST['base64'] ?? false) {
-            die('data:image/png;base64,'.$browser->base64Screenshot());
+            die("data:image/{$type};base64,".$browser->base64Screenshot());
         }
+
+        $file = Uuid::uuid4().".{$type}";
 
         header("Cache-Control: public");
         header("Content-Description: File Transfer");
         header("Content-Disposition: attachment; filename=$file");
-        header("Content-Type: image/png");
+        header("Content-Type: image/{$type}");
         header("Content-Transfer-Encoding: binary");
         echo $browser->screenshot();
         die();
